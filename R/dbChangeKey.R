ccd1d2340045c58dd156ff1c5236e47c997bab88
Bruno Gonçalves <bpvgoncalves@users.noreply.github.com>
2023-02-03 16:30:50 +0100

    Add function to allow changing the database key.

---
+++ R/dbChangeKey.R
@@ -0,0 +1,30 @@
+#' dbChangeKey
+#'
+#' Changes the key for an encrypted database.
+#'
+#' @param conn     database Connection
+#' @param old_key  Old database key, a character string of size 64, containing
+#'   32 hex encoded characters currently used as key for the database.
+#' encryption.
+#' @param new_key  New database key, a character string of size 64, containing
+#'   32 hex encoded characters to be used as new key for the database.
+#'
+#' @usage NULL
+#' @export
+dbChangeKey <- function(conn, old_key = NULL, new_key = NULL) {
+
+  dbExecute(conn, sprintf("PRAGMA key = \"x'%s'\";", old_key))
+  if (is.character(new_key)) {
+    if (nchar(new_key) == 64) {
+      dbExecute(conn, sprintf("PRAGMA rekey = \"x'%s'\";", new_key))
+    } else {
+      message("Cannot change database key. The 'new_key' provided has invalid length.")
+      invisible(FALSE)
+    }
+  } else {
+    message("Cannot change database key. The 'new_key' provided has invalid type.")
+    invisible(FALSE)
+  }
+
+  invisible(TRUE)
+}
