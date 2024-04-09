013694ab6404c8145b553c920933bfb7d00b8bbf
Bruno Gonçalves <bpvgoncalves@users.noreply.github.com>
2024-04-10 00:26:05 +0200

    Add function to remove encryption from database.

---
+++ tests/testthat/test-databaseKeyRemove.R
@@ -0,0 +1,18 @@
+test_that("it is posible to remove a key", {
+  on.exit({
+    dbDisconnect(con)
+    dbDisconnect(con2)
+  })
+
+  key <- "0123456789ABCDEF0123456789ABCDEF0123456789ABCDEF0123456789ABCDEF"
+  tmp_file <- tempfile()
+  con <- dbConnect(SQLCipher(), tmp_file, key = key)
+  dbWriteTable(con, "mtcars", mtcars)
+
+  newDB <- databaseKeyRemove(con)
+  expect_true(newDB$result)
+
+  con2 <- dbConnect(SQLCipher(), newDB$file)
+  expect_identical(dbListTables(con), dbListTables(con2))
+
+})
