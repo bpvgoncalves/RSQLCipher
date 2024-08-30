d7fb912c9af8a7afcf2041289e4e349222cbb35d
Bruno Gonçalves <bpvgoncalves@users.noreply.github.com>
2024-08-31 00:01:40 +0200

    Change key management functions (Add)

--- tests/testthat/test-databaseKeyAdd.R
+++ tests/testthat/test-databaseKeyAdd.R
@@ -14,8 +14,27 @@ test_that("it is posible to add a key", {
 
   con2 <- dbConnect(SQLCipher(), newDB$file, key = key)
   expect_identical(dbListTables(con), dbListTables(con2))
-
 })
+
+
+test_that("it is posible to add a password", {
+  on.exit({
+    dbDisconnect(con)
+    dbDisconnect(con2)
+  })
+
+  key <- "new_pass"
+  tmp_file <- tempfile()
+  con <- dbConnect(SQLCipher(), tmp_file)
+  dbWriteTable(con, "mtcars", mtcars)
+
+  newDB <- databaseKeyAdd(con, key)
+  expect_true(newDB$result)
+
+  con2 <- dbConnect(SQLCipher(), newDB$file, key = key)
+  expect_identical(dbListTables(con), dbListTables(con2))
+})
+
 
 test_that("fails with invalid key", {
   on.exit({
@@ -26,16 +45,21 @@ test_that("fails with invalid key", {
   con <- dbConnect(SQLCipher(), tmp_file)
   dbWriteTable(con, "mtcars", mtcars)
 
-  # wrong size, right type
-  key <- "0123456789ABCDEF0123456789ABCDEF012345"
+  # wrong type
+  key <- NULL
   expect_warning(newDB <- databaseKeyAdd(con, key),
-                 "invalid length")
+                 "is not valid")
+  expect_false(newDB$result)
+
+  key <- NA
+  expect_warning(newDB <- databaseKeyAdd(con, key),
+                 "is not valid")
   expect_false(newDB$result)
 
   # right size, wrong type (not hex)
-  key <- "0123456789ABCDEF0123456789ABCDEF0123456789ABCDEF0123456789ABCDEG"
+  key <- 1234
   expect_warning(newDB <- databaseKeyAdd(con, key),
-                 "invalid type")
+                 "is not valid")
   expect_false(newDB$result)
 
 })
